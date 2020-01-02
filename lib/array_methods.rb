45fed11baf4bccd566df95c9406ccd34d29cafaa
Sekou Dosso <sekou.dosso82@gmail.com>
2020-01-02 15:46:28 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,21 +5,23 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  l = array.length 
-  if l<2 
-    return nil 
+  
+  # if l<2 
+  #   return nil 
     
-  elsif l<3 
-        max = array[1]
-        min = array[0]
-        if min>max 
-          max = array[0]
-          min = array[1]
-        elsif min == max
-          return nil 
-        end 
-  elsif l>2 
+  # elsif l<3 
+  #       max = array[1]
+  #       min = array[0]
+  #       if min>max 
+  #         max = array[0]
+  #         min = array[1]
+  #       elsif min == max
+  #         return nil 
+  #       end 
+  # elsif l>2 
+  
   # [0,3,1,5,8,2,9]
+  l = array.length 
   max = array[0]
   min = 0
   counter = 0 
