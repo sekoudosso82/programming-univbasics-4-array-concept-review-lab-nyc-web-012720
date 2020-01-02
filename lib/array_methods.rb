3794e4f481cc405b2545d758c04dd8d96e61a90e
Sekou Dosso <sekou.dosso82@gmail.com>
2020-01-02 16:18:25 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -19,5 +19,15 @@ counter = 1
 end 
 
 def find_min_value(array)
-  # Add your solution here
+  # [0,3,1,5,8,2,9]
+min = array[0]
+l = array.length 
+counter = 1
+  while counter < l
+    if (array[counter]<min)
+        min = array[counter]
+    end 
+    counter +=1 
+  end 
+  min   
 end
