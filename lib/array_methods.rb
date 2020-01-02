b530416abafa7398e0be6f80c34f2159d0d9c538
Sekou Dosso <sekou.dosso82@gmail.com>
2020-01-02 16:21:09 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,33 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  
+  array.find_index(value_to_find)
+
 end
 
 def find_max_value(array)
-  # Add your solution here
-end
+# [0,3,1,5,8,2,9]
+max = array[0]
+l = array.length 
+counter = 1
+  while counter < l
+    if (array[counter]>max)
+        max = array[counter]
+    end 
+    counter +=1 
+  end 
+  max  
+end 
 
 def find_min_value(array)
-  # Add your solution here
+  # [0,3,1,5,8,2,9]
+min = array[0]
+l = array.length 
+counter = 1
+  while counter < l
+    if (array[counter]<min)
+        min = array[counter]
+    end 
+    counter +=1 
+  end 
+  min   
 end
